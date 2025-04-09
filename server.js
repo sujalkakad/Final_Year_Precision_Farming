5b59b76f26e5b3a78a5fb9e5c0419c681a3ff44f
sujalkakad <114797603+sujalkakad@users.noreply.github.com>
2025-04-09 22:57:38 +0530

    Fix: add axios to dependencies

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
 
-  const express = require("express");
+const express = require("express");
 const cors = require("cors");
+
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const multer = require("multer");
@@ -14,15 +15,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// ✅ Middleware
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://Precision-Farming-frontend.vercel.app"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use(cors());
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(bodyParser.json());
